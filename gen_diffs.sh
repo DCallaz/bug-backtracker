e955a7f144466105570dfa01764e4e445f63ab75
Dylan Callaghan <21831599@sun.ac.za>
2023-11-29 10:05:19 +0200

    Changed diff generation to commit diffs.
    [Details]
    Previously the diff generator generated diffs by diffing two consecutive
    versions. This is sufficient if the commit history is linear, but
    branching will confuse this strategy. Instead, we now produce commit
    diffs and use these for backtracking.
    
    [File's changed]
    
    	modified:   ../gen_diffs.sh

--- gen_diffs.sh
+++ gen_diffs.sh
@@ -3,21 +3,25 @@
 # start and end commit SHA's, this script will produce an all_shas SHAFILE and
 # a directory of relevant diffs.
 if [ "$1" ] && [ "$2" ]; then
-  shas=$(git log --format="%H" | awk "/$1/,/$2/;/$2/{getline;print;}")
+  shas=$(git log --format="%H" | awk "/$1/,/$2/")
 else
   shas=$(git log --format="%H")
 fi
 #echo "$shas"
-newer_shas=($shas)
-older_shas=("${newer_shas[@]}")
+#newer_shas=($shas)
+#older_shas=("${newer_shas[@]}")
 #unset 'older_shas[0]'
 #unset 'newer_shas[${#newer_shas[@]}-1]'
-older_shas=("${newer_shas[@]:1:${#newer_shas[@]}}")
-newer_shas=("${newer_shas[@]::${#newer_shas[@]}-1}")
+#older_shas=("${newer_shas[@]:1:${#newer_shas[@]}}")
+#newer_shas=("${newer_shas[@]::${#newer_shas[@]}-1}")
+if [ -d diffs ]; then
+  rm -r diffs/
+fi
 mkdir diffs
 > all_shas
-for i in $(seq 0 $( expr ${#newer_shas[@]} - 1 )); do
+for sha in $shas; do
   #echo newer ${newer_shas[$i]} older ${older_shas[$i]}
-  git diff -l10000 ${newer_shas[$i]} ${older_shas[$i]} > diffs/${newer_shas[$i]}.diff
-  echo ${newer_shas[$i]} >> all_shas
+  #git show -R --src-prefix="b/" --dst-prefix="a/" -l10000 --format="" $sha > diffs/$sha.diff
+  git show -R -l10000 --format="" $sha > diffs/$sha.diff
+  echo $sha >> all_shas
 done
