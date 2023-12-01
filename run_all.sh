fabd8e724db8b65e31a324257e663c6f5fd0ea2a
Dylan Callaghan <21831599@sun.ac.za>
2023-12-01 20:53:24 +0200

    Added file type changing in run_all script.
    [Details]
    Allowed the run_all script to change the file type for the backtracking
    algorithm based on the dataset.
    
    [File's changed]
    
    	modified:   defects4j/Chart_backtrack_2.json
    	modified:   defects4j/Closure_backtrack_2.json
    	modified:   defects4j/Lang_backtrack_2.json
    	modified:   defects4j/Math_backtrack_2.json
    	modified:   defects4j/Time_backtrack_2.json
    	modified:   run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-USAGE="./run_all.sh [-d <save dir>] [-f <save file suffix>] [-m diffs/lines] <projects file>"
-savedir="bugsinpy"
+USAGE="./run_all.sh [-d <dataset (bugsinpy/defects4j)>] [-s <save dir>] [-f <save file suffix>] [-m diffs/lines] <projects file>"
+dataset="bugsinpy"
 savefile="backtrack_2"
 mode="diffs"
-while getopts ":hd:f:m:" opt; do
+while getopts ":hd:s:f:m:" opt; do
   case ${opt} in
     d )
+      dataset="$OPTARG"
+      ;;
+    s )
       savedir="$OPTARG"
       ;;
     f )
@@ -25,12 +28,23 @@ while getopts ":hd:f:m:" opt; do
   esac
 done
 shift $((OPTIND -1))
+if [ ! "$savedir" ]; then
+  savedir="$dataset"
+fi
 projects_file="$1"
 projects_file="$(readlink -f "$(echo ${projects_file/"~"/~})")"
 if [ $# -lt 1 ] || [ ! -f "$projects_file" ]; then
   echo "$USAGE"
   echo "Please provide a valid projects file"
   exit 0
+fi
+if [ "$dataset" == "defects4j" ]; then
+  file_type=".java"
+elif [ "$dataset" == "bugsinpy" ]; then
+  file_type=".py"
+else
+  echo "ERROR: unknown dataset: $dataset"
+  exit 1
 fi
 for project in $(cat "$projects_file"); do
   echo "$project"
@@ -46,4 +60,5 @@ for project in $(cat "$projects_file"); do
     echo "ERROR: unknown mode: $mode"
     exit 1
   fi
-  java -jar backtrack.jar --file-type ".py" $(< $savedir/${project}_src.txt) $mode $all_shas $diffs $shas > $out; done
+  java -jar backtrack.jar --file-type $file_type $(< $savedir/${project}_src.txt) $mode $all_shas $diffs $shas > $out
+done
